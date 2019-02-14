0887d7b02b0963b74d96445d9d1f059f98a8b930
James Stidard <james@wave-venture.com>
2019-02-14 15:18:12 +0000

    upload command

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+import os
 import re
-
+import sys
 from distutils.core import setup
+from shutil import rmtree
+
+from setuptools import Command
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 
 with open('sanic_envconfig/__init__.py', 'r') as fd:
@@ -11,6 +17,44 @@ test_requirements = [
     'pytest',
     'pytest-mock',
     'pytest-cov']
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPi via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(version))
+        os.system('git push --tags')
+
+        sys.exit()
+
 
 setup(
     name='sanic_envconfig',
@@ -30,4 +74,9 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',))
+        'Programming Language :: Python :: 3.6',
+    ),
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
